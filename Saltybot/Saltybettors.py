d773778203d64c90960aab7696654164b7338bab
Ubuntu <ubuntu@ip-172-31-46-44.us-west-2.compute.internal>
2017-01-12 07:38:58 +0000

    Add CSS selectors

--- Saltybot/Saltybettors.py
+++ Saltybot/Saltybettors.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from Saltymodule import check_exists_by_xpath, clean_send_keys #, get_web_address
+from Saltymodule import check_exists_by_css, clean_send_keys #, get_web_address
 import SaltyDiscord
 from pyvirtualdisplay import Display
 from pymongo import MongoClient
@@ -39,13 +39,13 @@ class Saltybettor(object):
         try:
             timeout_secs = 300
             driver.get(Saltybettor.salty_url+"authenticate?signin=1")
-            username_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//input[@id='email'][@type='text']")))
-            password_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//input[@id='pword'][@type='password']")))
+            username_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='email'][type='text']")))
+            password_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[id='pword'][type='password']")))
             clean_send_keys(username, username_field)
             clean_send_keys(password, password_field)
-            submit = driver.find_element_by_xpath("//input[@value='Sign In']") # this is probably already there if fields are there
+            submit = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR,"input[value='Sign In']")))
             submit.click()
-            member_name_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//li[@class='nav-image']/h2/span[@class='navbar-text']")))
+            member_name_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.nav-image>h2>span.navbar-text")))
             self.name = member_name_field.text.split('(')[0].strip()
             self.pause_media()
             print(self.name + " logged in.")
@@ -68,14 +68,14 @@ class Saltybettor(object):
             print("Exception disabling media")
 
     def check_is_tournament(self):
-        return (check_exists_by_xpath(self.driver, "//span[@id='tournament-note']") or check_exists_by_xpath(self.driver, "//span[@id='balance'][contains(@class, 'purpletext')]"))
+        return (check_exists_by_css(self.driver, "span#tournament-note") or check_exists_by_css(self.driver, "span#balance[class *= 'purpletext']"))
 
     def get_balance(self, driver):
         timeout_secs = 1800
         balance_text = ""
         try:
             while True:
-                balance_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//span[@id='balance'][contains(@class, 'dollar')]")))
+                balance_field = WebDriverWait(driver, timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span#balance[class *= 'dollar']")))
                 balance_text = str(balance_field.text).replace(',','')
                 if re.search("[0-9]+",balance_text): # TODO: can you just add this check in the implicit wait above?
                     break
@@ -90,15 +90,11 @@ class Saltybettor(object):
             time.sleep(10)
             return -1
 
-#    def get_wager_field(self, driver):
-#        return driver.find_element_by_xpath("//input[@id='wager'][contains(@style, 'inline')]")
-    
-
     def input_bet_amount(self, driver, bet_amount):
         bet_timeout_secs = 1800
 
         try:
-            wager_field = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//input[@id='wager'][contains(@style, 'inline')]")))
+            wager_field = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#wager[style *= 'inline']")))
             clean_send_keys(str(int(bet_amount)), wager_field)
         except TimeoutException:
             print("Timeout waiting for wager field.")
@@ -106,7 +102,7 @@ class Saltybettor(object):
     def bet_player_red(self, driver):
         try:
             bet_timeout_secs = 1800
-            red_button = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//input[@id='player1']")))
+            red_button = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#player1")))
             red_button.click()
         except ElementNotVisibleException:
             pass
@@ -114,14 +110,14 @@ class Saltybettor(object):
     def bet_player_blue(self, driver):
         try:
             bet_timeout_secs = 1800
-            blue_button = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.XPATH, "//input[@id='player2']")))
+            blue_button = WebDriverWait(driver, bet_timeout_secs).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#player2")))
             blue_button.click()
         except ElementNotVisibleException:
             pass
 
     def check_bet_successful(self, driver):
         try:
-            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//span[@id='betconfirm'][@class='greentext']")))
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span#betconfirm.greentext")))
         except TimeoutException:
             print("Not sure if bet made it in time.")
         else:
@@ -157,7 +153,7 @@ class Saltybettor(object):
                 self.driver.refresh()
 
     def check_able_to_bet(self):
-        return (check_exists_by_xpath(self.driver, "//input[@id='wager'][contains(@style, 'inline')]") and not check_exists_by_xpath(self.driver, "//span[@id='betconfirm'][@class='greentext']"))
+        return (check_exists_by_css(self.driver, "input#wager[style *= 'inline']") and not check_exists_by_css(self.driver, "span#betconfirm.greentext"))
 
     def display_bet_preparation(self):
         print("====")
--- Saltybot/Saltymodule.py
+++ Saltybot/Saltymodule.py
@@ -2,7 +2,7 @@
 
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-
+import os, re
 def clean_send_keys(text, element):
     element.click()
     element.clear()
@@ -14,3 +14,16 @@ def check_exists_by_xpath(driver, xpath):
     except NoSuchElementException:
         return False
     return True
+
+def check_exists_by_css(driver,css_selector):
+    try:
+        driver.find_element_by_css_selector(css_selector)
+    except NoSuchElementException:
+        return False
+    return True
+
+def get_web_address():
+	public_ip=os.popen('wget http://ipinfo.io/ip -qO - | sed s/\\\\./-/g').read().rstrip('\n')
+	region=os.popen('cat /etc/resolv.conf | grep us').read().rstrip('\n').split(' ')[1].split('.')[0]
+	return "ec2-" + public_ip + "." + region + ".compute.amazonaws.com:3007/bethistory"
+
